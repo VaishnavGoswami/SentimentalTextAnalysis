7df9d53ec806681d718190bdb364d5805659ee5d
VaishnavGoswami <77101474+VaishnavGoswami@users.noreply.github.com>
2022-05-22 00:05:31 +0530

    Add files via upload

--- linkcapture.py
+++ linkcapture.py
@@ -1,8 +1,5 @@
-import copyreg
-from bs4 import BeautifulSoup
-from numpy import size # this module helps in web scrapping.
+from bs4 import BeautifulSoup # this module helps in web scrapping.
 import requests  # this module helps us to download a web page
-from csv import writer
 import csv
 import re
 
@@ -15,6 +12,8 @@ def ListofList(lst):
       
     return(res)
 
+
+# web scrapping the web sites for blog links 
 headers = {'User-Agent': 'My User Agent 1.0',
         #''Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'
     }
@@ -24,14 +23,16 @@ r  = requests.get(url, headers=headers)
 data = r.text
 soup = BeautifulSoup(data, "html.parser")
 # print(soup)
+# manually identify the tag which contains blog links and edit in find_all method below
 links_data=soup.find_all("a", class_="full-link")
-# print (type(links_data))
+# Extract strings that contain link from tags
 links = re.findall(r'"(.*?)(?<!\\)"', str(links_data))
-# print(type(links))
+
+#remove unwanted strings which are not links by adding them to "extras" lists also removes dupliates (if any)
 final_links = []
 extras = ['full-link', '_blank', 'full-link']
 for i in links:
-    if (i not in extras):
+    if (i not in extras and i not in final_links):
         final_links.append(i)
 
 #converting list to list of lists
@@ -43,18 +44,10 @@ for link in final_links:
     link.insert(0,count)
     count += 1
 
-# file = open("InputSheet.csv", "w", newline= "")
-# header = ['URL_ID', 'URL']
-# writer = csv.writer(file, fieldnames = header)
-
-# for w in range(len(final_links)) :
-#   writer.writerow([w+1, final_links[w]])
-
-# file.close()
 
 file = open('InputSheet.csv', 'a+', newline ='')
  
-# writing the data into the file
+# writing the data into the CSV file
 with file:   
     write = csv.writer(file)
     write.writerows(final_links)
@@ -62,4 +55,3 @@ with file:
 file.close
 
 
-# https://blog.myntra.com/rubans-a-story-of-sheer-grit-and-perseverance/
